8af9efab9356dc52fca51ca775196d2d0bda9a26
Amy MacKinnon <amy.mackinnon@coupa.com>
2014-03-11 16:25:55 -0700

    Added relationship model and tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,22 +2,41 @@ class User < ActiveRecord::Base
   has_secure_password
   attr_accessible :name, :email, :password, :password_confirmation
 
-  before_save { |user| user.email = email.downcase }
   before_save :create_remember_token
 
   has_many :tweets, dependent: :destroy
+  has_many :relationships, foreign_key: "follower_id", dependent: :destroy
+  has_many :reverse_relationships, foreign_key: "followed_id",
+                                   class_name: "Relationship",
+                                   dependent: :destroy
+  has_many :followed_users, through: :relationships, source: :followed
+  has_many :followers, through: :reverse_relationships, source: :follower
 
   valid_email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
   validates :email, presence: true,
-                    uniqueness: true,
+                    uniqueness: { case_sensitive: false },
                     format: { with: valid_email_regex }
-  validates :name, presence: true, uniqueness: true
+  validates :name, presence: true, uniqueness: { case_sensitive: false }
   validates :password, presence: true, length: {minimum: 6}
   validates :password_confirmation, presence: true
 
+  # method for using name as the :id in route
   def to_param
     name
+  end
+
+  # methods for following other users
+  def following?(other_user)
+    relationships.find_by_followed_id(other_user.id)
+  end
+
+  def follow!(other_user)
+    relationships.create!(followed_id: other_user.id)
+  end
+
+  def unfollow!(other_user)
+    relationships.find_by_followed_id(other_user.id).destroy
   end
 
   private
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,61 +1,94 @@
 require 'spec_helper'
 
 describe User do
-  context 'valid user entry' do
-    it 'has a name, password, and email' do
-      expect(build(:user)).to be_valid
+  let(:user) { create(:user) }
+
+  subject { user }
+
+  describe 'class methods' do
+    it { should respond_to :authenticate }
+    it { should respond_to :relationships }
+    it { should respond_to :reverse_relationships }
+    it { should respond_to :followed_users }
+    it { should respond_to :tweets }
+    it { should respond_to :follow! }
+    it { should respond_to :following? }
+    it { should respond_to :unfollow! }
+  end
+
+  describe 'entry' do
+    context 'is valid' do
+      it 'has a name, password, and email' do
+        expect(user).to be_valid
+      end
+
+      it 'has a properly formatted email address' do
+        expect(build(:user, email: 'test.com')).to have(1).errors_on(:email)
+      end
+
+      it 'generates a :remember_token before saving' do
+        expect(user.remember_token).to_not eq(nil)
+      end
+
     end
 
-    it 'converts email to downcase before saving' do
-      user = create(:user, email: 'TEST@TEST.com')
-      expect(user.email).to eq('test@test.com')
-    end
+    context 'is invalid' do
+      it 'has no name' do
+        expect(build(:user, name: nil)).to have(1).errors_on(:name)
+      end
 
-    it 'has a properly formatted email address' do
-      expect(build(:user, email: 'test.com')).to have(1).errors_on(:email)
-    end
+      it 'has no email' do
+        expect(build(:user, email: nil)).to have(2).errors_on(:email)
+      end
 
-    it 'generates a :remember_token before saving' do
-      user = create(:user)
-      expect(user.remember_token).to_not eq(nil)
+      it 'is a duplicate email address' do
+        expect(build(:user, email: user.email)).to have(1).errors_on(:email)
+      end
+
+      it 'is a duplicate name' do
+        expect(build(:user, name: user.name)).to have(1).errors_on(:name)
+      end
+
+      it 'has no password' do
+        expect(build(:user, password: nil, password_confirmation: nil)).to have(2).errors_on(:password)
+      end
+
+      it 'has no password_confirmation' do
+        expect(build(:user, password_confirmation: nil)).to have(1).errors_on(:password_confirmation)
+      end
+
+      it 'has a password and password_confirmation that don\'t match' do
+        user2 = build(:user, password: 'testing', password_confirmation: 'foobar')
+        expect(user2).to have(1).errors_on(:password)
+      end
+
+      it 'has a password that is too short' do
+        expect(build(:user, password: 'foo', password_confirmation: 'foo')).to have(1).errors_on(:password)
+      end
     end
   end
 
-  context 'invalid user entry' do
-    it 'has no name' do
-      expect(build(:user, name: nil)).to have(1).errors_on(:name)
+  describe 'following' do
+    let(:other_user) { create(:user) }
+    before do
+      user.follow!(other_user)
     end
 
-    it 'has no email' do
-      expect(build(:user, email: nil)).to have(2).errors_on(:email)
+    it { should be_following(other_user) }
+    its(:followed_users) { should include(other_user) }
+
+    describe 'and unfollowing' do
+      before do
+        user.unfollow!(other_user)
+      end
+
+      it { should_not be_following(other_user) }
+      its(:followed_users) { should_not include(other_user) }
     end
 
-    it 'is a duplicate email address' do
-      user = create(:user, email: 'foo@bar.com')
-      expect(build(:user, email: 'foo@bar.com')).to have(1).errors_on(:email)
-    end
-
-    it 'is a duplicate name' do
-      user = create(:user, name: 'bob')
-      expect(build(:user, name: 'bob')).to have(1).errors_on(:name)
-    end
-
-    it 'has no password' do
-      expect(build(:user, password: nil, password_confirmation: nil)).to have(2).errors_on(:password)
-    end
-
-    it 'has no password_confirmation' do
-      expect(build(:user, password_confirmation: nil)).to have(1).errors_on(:password_confirmation)
-    end
-
-    it 'has a password and password_confirmation that don\'t match' do
-      user = build(:user, password: 'testing', password_confirmation: 'foobar')
-      expect(user).to have(1).errors_on(:password)
-    end
-
-    it 'has a password that is too short' do
-      expect(build(:user, password: 'foo', password_confirmation: 'foo')).to have(1).errors_on(:password)
+    describe 'followed user' do
+      subject { other_user }
+      its(:followers) { should include(user) }
     end
   end
-
 end
